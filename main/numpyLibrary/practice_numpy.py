465915b226fb899a3f8335945a14f84ff9659a12
LRLayan <rameshlayan4@gmail.com>
2024-12-25 14:45:24 +0530

    addition

--- main/numpyLibrary/practice_numpy.py
+++ main/numpyLibrary/practice_numpy.py
@@ -6,3 +6,11 @@ print(np.__version__) # check numpy version
 # -----------------------
 array_1 = np.array([1,2,3,4])
 print(array_1, ", array type : " , type(array_1))
+
+# -----------------------
+# the length of arrays is equals, otherwise comming a error
+arr_1 = np.array([10,20,30])
+arr_2 = np.array([20,10,40])
+
+# addition
+print(arr_1+arr_2)
