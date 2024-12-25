df9e63bfe535f8e55326f3e121adb86ff268ad4b
LRLayan <rameshlayan4@gmail.com>
2024-12-25 15:13:38 +0530

    3D array and shape purpose

--- main/numpyLibrary/practice_numpy.py
+++ main/numpyLibrary/practice_numpy.py
@@ -42,9 +42,22 @@ print(np.max(arr_1))
 # -----------------------
 # 2D arrays
 arr_3 = np.array([[10,20,30],[5,8,6]])
-arr_4 = np.array([[10,20,30],[5,8,6]])
 print(arr_3)
 print(arr_3.size)
 print(arr_3.shape) # (2, 3) 2-> the 2D array has two 1D arrys / 3-> one 1D array has 3 elements
+
+# -----------------------
+# 3D array
+arr_4 = np.array([[[10,20,30],[20,10,30]],[[100,50,150],[50,100,50]]])
+print(arr_4)
+print(arr_4.size)
+print(arr_4.shape) # (2, 2, 3)
+# (2, 2, 3)
+# 2 -> the 3D array has two 2D arrays
+# 2 -> the 2D array has two 1D arrays
+# 3 -> the 1D array has 3 elements
+
+# shape attribute
+#   > This property returns the dimension as a tuple in an array.
 
 
