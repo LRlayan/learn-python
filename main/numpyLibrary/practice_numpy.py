8d783e0b95bedae39279f1ab919ab685dbe5ae8a
LRLayan <rameshlayan4@gmail.com>
2024-12-25 14:45:00 +0530

    np.array()

---
+++ main/numpyLibrary/practice_numpy.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# -----------------------
+print(np.__version__) # check numpy version
+
+# -----------------------
+array_1 = np.array([1,2,3,4])
+print(array_1, ", array type : " , type(array_1))
