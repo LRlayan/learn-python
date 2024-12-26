f45933a21c4c912bac446865d75f03e03c27f13b
LRLayan <rameshlayan4@gmail.com>
2024-12-27 01:05:44 +0530

    how to get data in rows

--- main/pandasLibrary/practice_pandas.py
+++ main/pandasLibrary/practice_pandas.py
@@ -2,14 +2,34 @@ import pandas as pd
 
 # how to create Dataframe using python dictionary
 data = {
-    'name':['Alice','John'],
-    'age':[23,28]
+    'Name':['Alice','John'],
+    'Age':[23,28]
 }
 
-data = pd.DataFrame(data)
-print(data)
+data_frame = pd.DataFrame(data)
+print(data_frame)
     # name  age
 # 0  Alice   23
 # 1   John   28
 # 0,1 is the normal default index for identifying rows, you can change this index if you want. 
 
+# ---------------------------
+# how to get data in rows
+# You can get the value in relevent to the row index of header
+print(data_frame.loc[1,'Name']) # John
+
+# You can get the value in relevent to the row and colomn index
+print(data_frame.iloc[1,0]) # John
+print(data_frame.iloc[1,1]) # 28
+
+# you can get the value in relevent to the row index
+print(data_frame.loc[1]) 
+# name    John
+# age       28
+# Name: 1, dtype: object
+
+print(data_frame.loc[0].Name) # Alice
+print(data_frame.loc[0].Age) # 23
+
+# multiple rows can returns
+print(data_frame.loc[[0,1]])
